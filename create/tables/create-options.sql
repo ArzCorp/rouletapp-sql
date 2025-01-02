5a1b04dd1173ae3201132c601c976f4652bfd226
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2025-01-01 22:24:23 -0600

    🏷 update create table queries

--- create/tables/create-options.sql
+++ create/tables/create-options.sql
@@ -3,8 +3,10 @@ CREATE TABLE `rouletapp`.`options` (
   `option_name` VARCHAR(50) NOT NULL,
   `option_color` VARCHAR(100) NOT NULL,
   `roulette_id` INT NOT NULL,
+  `is_active` BOOLEAN DEFAULT TRUE,
   `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
   `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
+  `delete_at` TIMESTAMP,
   PRIMARY KEY (`option_id`),
   FOREIGN KEY (`roulette_id`) REFERENCES `roulettes`(`roulette_id`)
 );
--- create/tables/create-roulettes.sql
+++ create/tables/create-roulettes.sql
@@ -1,8 +1,10 @@
 CREATE TABLE `rouletapp`.`roulettes` (
   `roulette_id` INT NOT NULL AUTO_INCREMENT,
   `roulette_name` VARCHAR(50) NOT NULL,
-  `roulette_description` VARCHAR(100) NOT NULL,
+  `roulette_description` VARCHAR(100),
+  `is_active` BOOLEAN NOT NULL DEFAULT TRUE,
   `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
   `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
+  `delete_at` TIMESTAMP,
   PRIMARY KEY (`roulette_id`)
 );
